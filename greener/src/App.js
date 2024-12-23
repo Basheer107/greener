f8e9e61f721eb8de1c400342ddbe12575244a00f
Basheer107 <mrhatela17@gmail.com>
2024-12-23 19:45:34 +0530

    lumpung changed from html to react

---
+++ greener/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.scss';
+import Home from './components/Home';
+import Footer from './components/Footer';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Home/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ greener/src/components/BestSellingCard.js
@@ -0,0 +1,18 @@
+
+function BestSellingCard ({cards}) {
+    return (
+        <div class="plantCard">
+        <div class="thumbnail">
+            <img src={cards.img} alt=""/>
+        </div>
+        <div class="info">
+            <h3>{cards.name}</h3>
+            <p>{cards.decs}</p>
+            <span class="price">{cards.price}</span>
+            <a href="" class="button">Buy now</a>
+        </div>
+    </div>
+    )
+}
+
+export default BestSellingCard
